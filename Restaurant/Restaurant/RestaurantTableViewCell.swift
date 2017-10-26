0b18e2c52cfbbf92a8ba2c9912df56cecb48b70b
Andrew Hinson <hinsonan@gmail.com>
2017-10-25 22:21:43 -0500

    Basic table cell layout

---
+++ Restaurant/Restaurant/RestaurantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantTableViewCell.swift
+//  Restaurant
+//
+//  Created by Andrew Hinson on 10/25/17.
+//  Copyright © 2017 Andrew Hinson. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
